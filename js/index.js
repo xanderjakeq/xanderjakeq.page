83516199ff1392a10bf9ca8a6cc14a6d2d9ec5cd
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-24 11:17:43 -0400

    Redesign projects section UX

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const portfolioDiv = document.querySelector('.portfolio')
-const workButton = document.querySelector('.workButton')
+const workButton = document.querySelector('.expand_button')
+const up_arrow = document.getElementById('up_arrow')
+const down_arrow = document.getElementById('down_arrow')
 
 workButton.addEventListener('click', (e) => {
     portfolioDiv.classList.toggle('active')
+    up_arrow.classList.toggle('hidden')
+    down_arrow.classList.toggle('hidden')
 })
