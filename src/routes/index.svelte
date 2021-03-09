0ffb48198387429fd015efc57550e28bb8e958d5
xanderjakeq <xanderjakeq@gmail.com>
2021-03-08 21:34:37 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,131 @@
+<script>
+	let styles = {
+		'v': '#f4ed2a',
+		'note-color': '#FF5555',
+		'bg': '#AAAAAA',
+	};
+
+	
+	$: cssVarStyles = Object.entries(styles)
+		.map(([key, value]) => `--${key}:${value}`)
+		.join(';')
+</script>
+
+<style>
+	.container {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+        align-items: center;
+
+        background-image: url("../images/briight.jpg");
+        opacity: .8;
+        background-size: cover;
+        background-position: center;
+	}
+
+    #heading_container {
+        width: fit-content;
+		margin: .5em 0;
+    }
+
+    #sub_container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: fit-content;
+        align-items: center;
+        padding: 1em;
+
+        background-color: rgba(250, 250, 250, 0.5);
+        border-radius: 5px;
+    }
+    
+
+    nav {
+        margin-top: 1em;
+    }
+
+    h1, h2 {
+        opacity: 1;
+    }
+
+    nav > a {
+        font-family: "Poppins";
+
+        font-weight: 700;
+        margin: 0 .5em;
+        text-decoration: none;
+        opacity: .8;
+
+        transition: all .2s;
+    }
+
+    nav > a:hover {
+        text-decoration: underline;
+        opacity: 1;
+    }
+
+    .letter_spacing_1 {
+        letter-spacing: 2vw;
+        margin-right: -2vw;
+    }
+
+    .letter_spacing_2 {
+        letter-spacing: .4em;
+        margin-right: -.5em;
+    }
+
+	h1 {
+        font-family: "Josefin Slab";
+		font-size: 2.8em;
+	/*	text-transform: uppercase; */
+		font-weight: 700;
+        text-align: center;
+	}
+    h2 {
+        font-family: "Poppins";
+        font-weight: semi-bold;
+        text-align: center;
+    }
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>xanderjakeq | home</title>
+</svelte:head>
+
+<div class="container">
+	<div id="sub_container">
+		<div id="heading_container">
+			<h1 class = "letter_spacing_1">xanderjakeq</h1>
+			<h2 class = "letter_spacing_2">Xander Jake de los Santos</h2>
+		</div>
+		<nav>
+			<a rel=external target="_blank" href="https://blog.xanderjakeq.page/">blog</a>
+			<a rel=external target="_blank" href="https://xanderjakeq.artstation.com/">art</a>
+            <a rel=external target="_blank" href="./sketches">sketches</a>
+			<a rel=external target="_blank" href="https://github.com/xanderjakeq">github</a>
+		</nav>
+	</div>
+</div>
---
+++ src/routes/sketches.svelte
@@ -0,0 +1,90 @@
+<svelte:head>
+	<title>Sketches</title>
+</svelte:head>
+
+<script>
+    import { onMount } from "svelte";
+    import InfiniteScroll from "svelte-infinite-scroll";
+    let sketches = [];
+
+    let user = "305874292397178882";
+    let before = "";
+    let hasMore = true;
+
+    const getSketches = async (user_id, earliest) => {
+        const res = await fetch("http://localhost:5000/api/sketches?" + new URLSearchParams({
+            user_id,
+            earliest
+        }));
+        const data = await res.json();
+        before = data.earliest;
+        hasMore = sketches.length < 50 && data.attachments.length > 0 ;
+
+        sketches = [...sketches, ...data.attachments];
+
+        if (sketches.length < 20) {
+            getSketches(user_id, data.earliest)
+        }
+    }
+
+    const handleMore = (e) => {
+        e.preventDefault()
+        getSketches(user, before)
+    }
+
+    onMount(async function() {
+        getSketches(user, before)
+    });
+</script>
+
+<style>
+    h1 {
+        font-family: "Josefin Slab";
+		font-size: 2.8em;
+	/*	text-transform: uppercase; */
+		font-weight: 700;
+        text-align: center;
+
+        margin: 30px 0;
+	}
+
+    #mainContainer {
+        display: flex;
+        justify-content: center;
+    }
+    
+    #classContainer {
+        display: grid;
+        max-width: 1000px;
+        grid-template-columns: repeat(3, 1fr); 
+        justify-items: center;
+        align-items: center;
+
+    }
+
+    img {
+        max-width: 300px;
+        max-height: 300px;
+        
+        overflow: hidden;
+
+        margin: 10px;
+    }
+
+    p {
+        font-family: "Poppins";
+        text-align: center;
+    }
+</style>
+
+<h1>Sketches</h1>
+
+<p>recent sketches</p>
+<div id = "mainContainer">
+    <div id="classContainer">
+    {#each sketches as sketch}
+            <img alt="sketch" src="{sketch.url}">
+    {/each}
+    </div>
+    <InfiniteScroll {hasMore} window={true} on:loadMore={handleMore}/>
+</div>
