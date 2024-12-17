8acaeadeec8cf8be182370cbd2cd1ded5cd2e44c
xanderjakeq <13279523+xanderjakeq@users.noreply.github.com>
2024-12-17 16:13:01 -0500

    add caching headers to static files

--- src/router.rs
+++ src/router.rs
@@ -1,4 +1,5 @@
 use crate::handlers::*;
+use salvo::caching_headers::CachingHeaders;
 use salvo::catcher::Catcher;
 use salvo::serve_static::StaticDir;
 use salvo::{Router, Service};
@@ -6,7 +7,7 @@ use salvo::{Router, Service};
 use crate::handlers::render_error_page;
 
 pub fn get_router() -> Service {
-    let router = Router::new()
+    let router = Router::with_hoop(CachingHeaders::new())
         .hoop(init_data)
         .get(render_index)
         .push(Router::with_path("<section>").get(render_section))
